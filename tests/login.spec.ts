8ec1d8e8054a501cbcb47e947aa4f322f43bd674
CristianM1987 <cristian.medina@usal.edu.ar>
2024-02-20 10:43:32 -0400

    Setup Architecture

---
+++ tests/login.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+/*
+test('has title', async ({ page }) => {
+  await page.goto('https://shop.demoqa.com/');
+
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://shop.demoqa.com/');
+});
+*/
